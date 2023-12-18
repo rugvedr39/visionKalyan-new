ce95aad8409ef90a842cc5d8202a4e55026b7c1e
Rugved Raikwar <78421561+rugvedr39@users.noreply.github.com>
2023-12-18 22:25:26 +0530

    Update updateUser.js

--- updateUser.js
+++ updateUser.js
@@ -10,74 +10,42 @@ const { MongoClient, ObjectId } = require('mongodb');
 const mongoURL = 'mongodb+srv://kalyanvision381:uykt2riskUeq2LIj@cluster0.9wscwrp.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'VisionKalyan_New';
 const credentials = require('./asstets/visionkalyan-aee28f9bb9ac.json');
-console.log(credentials);
 
-const jwtClient = new JWT({
-  email: credentials.client_email,
-  key: credentials.private_key,
-  scopes: ['https://www.googleapis.com/auth/drive'],
-});
-
-// Use a promise to ensure the access token is available
-const getAccessToken = async () => {
-  try {
-    const tokens = await jwtClient.authorize();
-    return tokens.access_token;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const connectToMongoDBWithRetry = async () => {
-  const maxRetries = 100; // Adjust the number of retries as needed
-  let currentRetry = 0;
-
-  while (currentRetry < maxRetries) {
-    try {
-      // Connect to MongoDB
-      const client = await MongoClient.connect(mongoURL);
-      return client;
-    } catch (error) {
-      console.error(`Error connecting to MongoDB (Attempt ${currentRetry + 1}/${maxRetries}):`, error);
-      currentRetry++;
-
-      // Wait for a certain period before the next retry (e.g., 5 seconds)
-      const retryDelay = 1000;
-      console.log(`Retrying in ${retryDelay / 1000} seconds...`);
-      await new Promise(resolve => setTimeout(resolve, retryDelay));
-    }
-  }
-
-  console.error(`Max retries (${maxRetries}) reached. Unable to establish MongoDB connection.`);
-  return null;
-};
-
-const redirectUri = credentials.web?.redirect_uris?.[0] || 'http://localhost:3000/callback';
-
-const oAuth2Client = new google.auth.OAuth2(
-  credentials.client_id,
-  credentials.client_secret,
-  redirectUri
-);
-
-async function main() {
-    try {
-      const accessToken = await getAccessToken();
-      oAuth2Client.setCredentials({ access_token: accessToken });
-  
-      // Continue with the rest of your code
-      // ...
-    } catch (error) {
-      console.error('Error getting access token:', error);
-    }
-  }
-  
-  // Call the async function
-  main();
-
-const drive = google.drive({ version: 'v3', auth: oAuth2Client });
 
 router.post('/upload/:userId', upload.single('image'), async (req, res) => {
+  const jwtClient = new JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  });
+  // Use a promise to ensure the access token is available
+  const getAccessToken = async () => {
+    try {
+      const tokens = await jwtClient.authorize();
+      return tokens.access_token;
+    } catch (error) {
+      throw error;
+    }
+  };
+  const redirectUri = credentials.web?.redirect_uris?.[0] || 'http://localhost:3000/callback';
+  const oAuth2Client = new google.auth.OAuth2(
+    credentials.client_id,
+    credentials.client_secret,
+    redirectUri
+  );
+  async function main() {
+      try {
+        const accessToken = await getAccessToken();
+        oAuth2Client.setCredentials({ access_token: accessToken });
+        // Continue with the rest of your code
+        // ...
+      } catch (error) {
+        console.error('Error getting access token:', error);
+      }
+    }
+    // Call the async function
+    main();
+  const drive = google.drive({ version: 'v3', auth: oAuth2Client });
   try {
     // Upload to Google Drive
     const driveRes = await drive.files.create({
@@ -139,5 +107,29 @@ router.post('/upload/:userId', upload.single('image'), async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+const connectToMongoDBWithRetry = async () => {
+  const maxRetries = 100; // Adjust the number of retries as needed
+  let currentRetry = 0;
+
+  while (currentRetry < maxRetries) {
+    try {
+      // Connect to MongoDB
+      const client = await MongoClient.connect(mongoURL);
+      return client;
+    } catch (error) {
+      console.error(`Error connecting to MongoDB (Attempt ${currentRetry + 1}/${maxRetries}):`, error);
+      currentRetry++;
+
+      // Wait for a certain period before the next retry (e.g., 5 seconds)
+      const retryDelay = 1000;
+      console.log(`Retrying in ${retryDelay / 1000} seconds...`);
+      await new Promise(resolve => setTimeout(resolve, retryDelay));
+    }
+  }
+
+  console.error(`Max retries (${maxRetries}) reached. Unable to establish MongoDB connection.`);
+  return null;
+};
 
 module.exports = router;
