4c92f5c5225966eadaf8c2ea698d3a8360f5dcbc
rugvedr39 <rugvedr39@gmail.com>
2024-02-04 13:22:37 +0530

    payout routes updated

--- app.js
+++ app.js
@@ -16,12 +16,13 @@ const projects = require('./land-project');
 const emi = require('./emi');
 const updateUser = require('./updateUser');
 const extraemi = require('./extraEMI');
+require('dotenv').config();
 var morgan = require('morgan')
-
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 
+console.log('SENDGRID_API_KEY:', process.env.SENDGRID_API_KEY);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
@@ -175,8 +176,9 @@ app.get('/all-epins', async (req, res) => {
 
 
   app.get('/topusers', async (req, res) => {
+    console.log('heelo');
     try {
-      const client =connectToMongoDBWithRetry()
+      const client = await connectToMongoDBWithRetry()
       const db = client.db(dbName);
   
       const excludedUsernames = ['VK24496086', 'VK53912943'];
@@ -210,7 +212,7 @@ app.get('/all-epins', async (req, res) => {
       res.json({ success: true, topUsers: topUsers });
       client.close();
     } catch (error) {
-      // console.error('Error:', error);
+      console.error('Error:', error);
     } finally {
       await client.close();
     }
@@ -229,3 +231,142 @@ app.use('/extraemi', extraemi);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+
+
+const fetchDataAndGenerateExcel = async () => {
+  const url = 'mongodb+srv://kalyanvision381:uykt2riskUeq2LIj@cluster0.9wscwrp.mongodb.net/?retryWrites=true&w=majority';
+  const dbName = 'VisionKalyan_New';
+
+  try {
+    // Connect to the MongoDB database
+    const client = await connectToMongoDBWithRetry()
+    const db = client.db(dbName);
+
+    // Fetch all unpaid income
+    const indirectIncomeCollectionName = 'indirectIncomeCollection';
+    const indirectIncomeCollection = db.collection(indirectIncomeCollectionName);
+    const allUnpaidIncome = await indirectIncomeCollection.find({ status: 'unpaid' }).toArray();
+    const unpaidIds = allUnpaidIncome.map(entry => entry._id);
+
+    // Calculate total amounts by user
+    const totalAmountsByUser = allUnpaidIncome.reduce((result, entry) => {
+      const { username, amount, _id } = entry;
+      const existingUser = result.find(userEntry => userEntry.username === username);
+
+      if (existingUser) {
+        existingUser.amount += amount;
+        existingUser.ids.push(_id);
+      } else {
+        result.push({ username, amount, ids: [_id] });
+      }
+
+      return result;
+    }, []);
+
+    // Fetch user details for each user
+    const collectionName = 'users';
+    const collection = db.collection(collectionName);
+
+    for (const userEntry of totalAmountsByUser) {
+      const { username } = userEntry;
+      const userDetails = await collection.findOne({ username });
+      userEntry.userDetails = userDetails;
+    }
+
+    console.log(totalAmountsByUser, unpaidIds);
+
+    const totalAmountsByUser1 = totalAmountsByUser.map((entry) => ({
+      Name: entry.userDetails?.name,
+      username: entry.userDetails?.username,
+      'Bank Name': entry.userDetails?.bankDetails?.bankName,
+      'Bank IFSC Code': entry.userDetails?.bankDetails?.ifscCode,
+      'Bank Account Number': entry.userDetails?.bankDetails?.accountNumber,
+      'PAN Number': entry.userDetails?.panNumber,
+      'Total Income': entry.amount,
+      'TDS': entry.amount*0.05,
+      'Net Payable': entry.amount-entry.amount*0.05,
+      ids:entry.ids,
+      "Date": new Date()
+    }));
+    // Close the database connection
+
+
+    // Generate Excel file
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Total Amounts');
+
+    // Add header row
+    worksheet.addRow(['Name', 'Bank Name', 'Bank IFSC Code', 'Bank Account Number', 'Pan Number', 'Amount', 'TDS', 'Net Payable']);
+
+    // Add data rows
+    for (const userEntry of totalAmountsByUser) {
+      const { userDetails, amount } = userEntry;
+      const { name, bankDetails, panNumber } = userDetails;
+
+      // Assuming bankDetails is an array with an object containing bank information
+      const { bankName, ifscCode, accountNumber } = bankDetails || {};
+
+      // Calculate TDS and Net Payable
+      const tds = 0.05 * amount; // 5% of Total Income
+      const netPayable = amount - tds;
+
+      worksheet.addRow([name, bankName, ifscCode, accountNumber, panNumber, amount, tds, netPayable]);
+    }
+
+    // Save Excel file
+    await db.collection('PaymentProccedDetails').insertOne({unpaidIds,data:totalAmountsByUser1});
+    const objectIdsToUpdate = unpaidIds.map(id => new ObjectId(id));
+    await db.collection('indirectIncomeCollection').updateMany(
+      { _id: { $in: objectIdsToUpdate } },
+      { $set: { status: 'Payment Under Process' } }
+      );
+      await client.close();
+
+
+    const currentDate = new Date().toLocaleDateString('en-GB').replace(/\//g, '_');  
+    const excelFileName = `Payout_Summary_${currentDate}.xlsx`;
+    await workbook.xlsx.writeFile(excelFileName);
+
+    const attachmentContent = fs.readFileSync(excelFileName, 'base64');
+    sgMail.setApiKey('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');
+
+    const msg = {
+          to: 'rugvedr39@gmail.com',
+          from: 'rugved.developer@gmail.com',
+          subject: 'Report From Vision Kalyan',
+          text: '',
+          attachments: [
+            {
+              content: attachmentContent,
+              filename: excelFileName,
+              type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+              disposition: 'attachment',
+            },
+          ],
+        };
+
+        sgMail.send(msg)
+        .then(() => {
+          console.log('Email sent successfully');
+          fs.unlinkSync(excelFileName);
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error.response.body);
+        });
+
+    console.log('Excel file generated successfully: TotalAmountsWithTDS.xlsx');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+
+cron.schedule('0 20 * * *', async () => {
+  console.log('Running cron job...');
+  await fetchDataAndGenerateExcel();
+  console.log('Cron job completed.');
+});
+
--- payoutRoutes.js
+++ payoutRoutes.js
@@ -4,13 +4,13 @@ const express = require('express');
 const router = express.Router();
 const { MongoClient, ObjectId } = require('mongodb');
 
-const url = 'mongodb+srv://kalyanvision381:uykt2riskUeq2LIj@cluster0.9wscwrp.mongodb.net/?retryWrites=true&w=majority';
+const mongoURL = 'mongodb+srv://kalyanvision381:uykt2riskUeq2LIj@cluster0.9wscwrp.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'VisionKalyan_New';
 
 const sdk = require('api')('@waapi/v1.0#ehy7f2rlp03cxd0');
 sdk.auth('t0wD644lvq413rmF02Hx2TRpdOhBTmsd6Z1KjmIM');
 
-const createEMIMessage = (recipientName, accountID, pendingEMIAmount , bankAccount) => {
+const createEMIMessage = (recipientName, accountID, pendingEMIAmount, bankAccount) => {
   return `
   Hi ${recipientName},
 
@@ -28,23 +28,15 @@ Best regards,
 Vision Kalyan`;
 };
 
-// Middleware to handle database connection
-const withDb = async (req, res, next) => {
-  try {
-    const client = new MongoClient(url);
-    await client.connect();
-    req.db = client.db(dbName);
-    next();
-  } catch (error) {
-    // console.error('Error connecting to the database:', error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
-  }
-};
-
 // Fetch payout details
-router.get('/payoutdetails', withDb, async (req, res) => {
+router.get('/payoutdetails', async (req, res) => {
   try {
-    const { db } = req;
+    const client = await connectToMongoDBWithRetry();
+    if (!client) {
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+    const db = client.db(dbName);
 
     // Fetch all unpaid income
     const indirectIncomeCollectionName = 'indirectIncomeCollection';
@@ -79,14 +71,18 @@ router.get('/payoutdetails', withDb, async (req, res) => {
 
     res.status(200).json({ totalAmountsByUser, unpaidIds });
   } catch (error) {
-    // console.error('Error fetching payout details:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
-router.post('/procced', withDb, async (req, res) => {
+router.post('/procced', async (req, res) => {
   try {
-    const { db } = req;
+    const client = await connectToMongoDBWithRetry();
+    if (!client) {
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+    const db = client.db(dbName);
     await db.collection('PaymentProccedDetails').insertOne(req.body);
     const objectIdsToUpdate = req.body.unpaidIds.map(id => new ObjectId(id));
     await db.collection('indirectIncomeCollection').updateMany(
@@ -95,34 +91,40 @@ router.post('/procced', withDb, async (req, res) => {
     );
     res.json({ success: true, message: 'Payments Procced' });
   } catch (error) {
-    // console.error('Error fetching payout details:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
-router.get('/get/procced', withDb, async (req, res) => {
+router.get('/get/procced', async (req, res) => {
   try {
-    const { db } = req;
+    const client = await connectToMongoDBWithRetry();
+    if (!client) {
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+    const db = client.db(dbName);
     const result = await db.collection('PaymentProccedDetails').find().toArray();
     res.json({ success: true, data: result });
   } catch (error) {
-    // console.error('Error fetching payout details:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
-
-router.post('/procced/paid', withDb, async (req, res) => {
+router.post('/procced/paid', async (req, res) => {
   try {
-    const { db } = req;
+    const client = await connectToMongoDBWithRetry();
+    if (!client) {
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+    const db = client.db(dbName);
     const PaymentProccedDetailsCollection = db.collection('PaymentProccedDetails');
     const UsersCollection = db.collection('users');
     const indirectIncomeCollection = db.collection('indirectIncomeCollection');
     const payoutpaymentscollections = db.collection('RecentPayments');
-    let user = await UsersCollection.findOne({username:req.body.username});
+    let user = await UsersCollection.findOne({ username: req.body.username });
 
-    // const createEMIMessage = (recipientName, accountID, pendingEMIAmount , bankAccount) => {
-    const message = createEMIMessage(user.name,user.username,req.body['Net Payable'],user.bankDetails?.accountNumber)
+    const message = createEMIMessage(user.name, user.username, req.body['Net Payable'], user.bankDetails?.accountNumber);
     const objectIdsToUpdate = req.body.ids.map(id => new ObjectId(id));
 
     // Update IndirectIncomeCollection
@@ -138,7 +140,7 @@ router.post('/procced/paid', withDb, async (req, res) => {
     );
 
     // Insert into RecentPayments collection
-    
+
     // Check if data array is empty and delete the document
     const result1 = await PaymentProccedDetailsCollection.findOne({ _id: new ObjectId(req.body._id) });
     if (result1.data.length === 0) {
@@ -149,43 +151,48 @@ router.post('/procced/paid', withDb, async (req, res) => {
 
     const countryCode = '91';
     const formattedNumber = user.phoneNumber.startsWith('+') ? `${countryCode}${user.phoneNumber.slice(1)}` : `${countryCode}${user.phoneNumber}`;
-    // const formattedNumber = '918600988002';
     const response = await sdk.postInstancesIdClientActionSendMessage({
-        chatId: `${formattedNumber}@c.us`,
-        message,
-      }, { id: '3177' });
+      chatId: `${formattedNumber}@c.us`,
+      message,
+    }, { id: '3177' });
 
     res.status(200).json({ data: 'Successful' });
   } catch (error) {
-    // console.error('Error updating payment details:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
-
-router.get('/payment/done-get', withDb, async (req, res) => {
+router.get('/payment/done-get', async (req, res) => {
   try {
-    const { db } = req;
+    const client = await connectToMongoDBWithRetry();
+    if (!client) {
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+    const db = client.db(dbName);
     const payoutpaymentscollections = db.collection('RecentPayments');
-    
+
     const result = await payoutpaymentscollections.find().toArray();
-    
+
     res.status(200).json({ data: result });
   } catch (error) {
-    // console.error('Error fetching recent payments:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
-router.get('/payment/payout/:username', withDb, async (req, res) => {
+router.get('/payment/payout/:username', async (req, res) => {
   const username = req.params.username;
   try {
-    const { db } = req;
+    const client = await connectToMongoDBWithRetry();
+    if (!client) {
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+
+    const db = client.db(dbName);
     const payoutpaymentscollections = db.collection('RecentPayments');
     const result = await payoutpaymentscollections.find({ username }).toArray();
-    res.status(200).json({success: true, data: result });
+    res.status(200).json({ success: true, data: result });
   } catch (error) {
-    // console.error('Error fetching recent payments:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
@@ -200,17 +207,15 @@ async function connectToMongoDBWithRetry() {
       const client = await MongoClient.connect(mongoURL);
       return client;
     } catch (error) {
-      // console.error(`Error connecting to MongoDB (Attempt ${currentRetry + 1}/${maxRetries}):`, error);
       currentRetry++;
 
       // Wait for a certain period before the next retry (e.g., 5 seconds)
       const retryDelay = 1000;
-      // console.log(`Retrying in ${retryDelay / 1000} seconds...`);
       await new Promise(resolve => setTimeout(resolve, retryDelay));
     }
   }
 
-  // console.error(`Max retries (${maxRetries}) reached. Unable to establish MongoDB connection.`);
   return null;
 }
+
 module.exports = router;
