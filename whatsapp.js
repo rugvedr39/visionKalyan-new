a21e2f153723873e2e5e25a98aeeac791a6754b0
rugvedr39 <rugvedr39@gmail.com>
2024-03-09 22:05:37 +0530

    whatsaapp messagew added

--- whatsapp.js
+++ whatsapp.js
@@ -1,7 +1,8 @@
 // sendMessage.js
 const fs = require('fs');
-const { Client ,LocalAuth} = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const puppeteer = require('puppeteer');
 
 const SESSION_FILE_PATH = './session.json';
 
@@ -10,38 +11,44 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
 
-const client = new Client({
-    authStrategy: new LocalAuth({
-    clientId: "client-one"
-    }),
-    puppeteer: { headless: false },
-    // session: sessionCfg
-    })
+(async () => {
+    const browser = await puppeteer.launch({ headless: true });
 
-client.on('qr', (qr) => {
-    // Display the QR code in the terminal
-    console.log('Scan the QR code:');
-    qrcode.generate(qr, { small: true });
-});
+    const client = new Client({
+        authStrategy: new LocalAuth({
+            clientId: "client-one"
+        }),
+        puppeteer: {
+            browserWSEndpoint: await browser.wsEndpoint()
+        },
+        session: sessionCfg
+    });
 
-client.on('authenticated', (session) => {
-    console.log('AUTHENTICATED', session);
-});
+    client.on('qr', (qr) => {
+        // Display the QR code in the terminal
+        console.log('Scan the QR code:');
+        qrcode.generate(qr, { small: true });
+    });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+    client.on('authenticated', (session) => {
+        console.log('AUTHENTICATED', session);
+    });
 
-client.initialize();
+    client.on('ready', () => {
+        console.log('Client is ready!');
+    });
 
-async function sendMessage(number, message) {
-    try {
-        const chat = await client.getChatById(`${number}@c.us`);
-        chat.sendMessage(message);
-        console.log('Message sent successfully!');
-    } catch (error) {
-        console.error('Error occurred while sending message:', error);
+    client.initialize();
+
+    async function sendMessage(number, message) {
+        try {
+            const chat = await client.getChatById(`${number}@c.us`);
+            chat.sendMessage(message);
+            console.log('Message sent successfully!');
+        } catch (error) {
+            console.error('Error occurred while sending message:', error);
+        }
     }
-}
 
-module.exports = { sendMessage };
+    module.exports = { sendMessage };
+})();
