de2365521d1867b61a282c068b4bbacf1cac7cf8
rugvedr39 <rugvedr39@gmail.com>
2024-03-09 22:15:28 +0530

    error solved

--- whatsapp.js
+++ whatsapp.js
@@ -1,8 +1,8 @@
 // sendMessage.js
 const fs = require('fs');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client ,LocalAuth} = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const puppeteer = require('puppeteer');
+
 
 const SESSION_FILE_PATH = './session.json';
 
@@ -11,40 +11,35 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
 
+const client = new Client({
+    authStrategy: new LocalAuth({
+    clientId: "client-one"
+    }),
+    puppeteer: { headless: true },
+    // session: sessionCfg
+    })
+
+client.on('qr', (qr) => {
+    // Display the QR code in the terminal
+    console.log('Scan the QR code:');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
 async function sendMessage(number, message) {
     try {
-        const browser = await puppeteer.launch({ headless: true });
-        const client = new Client({
-            authStrategy: new LocalAuth({
-                clientId: "client-one"
-            }),
-            puppeteer: {
-                browserWSEndpoint: await browser.wsEndpoint()
-            },
-            session: sessionCfg
-        });
-
-        client.on('qr', (qr) => {
-            // Display the QR code in the terminal
-            console.log('Scan the QR code:');
-            qrcode.generate(qr, { small: true });
-        });
-
-        client.on('authenticated', (session) => {
-            console.log('AUTHENTICATED', session);
-        });
-
-        client.on('ready', () => {
-            console.log('Client is ready!');
-        });
-
-        await client.initialize();
-
         const chat = await client.getChatById(`${number}@c.us`);
-        await chat.sendMessage(message);
+        chat.sendMessage(message);
         console.log('Message sent successfully!');
-
-        await browser.close();
     } catch (error) {
         console.error('Error occurred while sending message:', error);
     }
